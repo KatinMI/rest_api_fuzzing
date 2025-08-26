3dd008f184ef8fd16883cd338c4ece21da30ca37
Maksim <119787336+KatinMI@users.noreply.github.com>
2025-08-26 20:31:30 +0300

    Update cargo_fuzz_scripts.sh

--- cargo_fuzz_scripts.sh
+++ cargo_fuzz_scripts.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 rustup +nightly component add llvm-tools-preview
 ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -sparse ./*.profraw -o merged.profdata
 ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov     show /home/maksim/rust-url/fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_target_1     --instr-profile=merged.profdata     --format=html     --output-dir=coverage-html     --show-line-counts-or-regions  --ignore-filename-regex='/.cargo/registry'
